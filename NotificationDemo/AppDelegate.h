aeaf408ff77aa116d41616c094d376c3738d0a95
Arthur <jjyy1106@qq.com>
2016-08-26 09:15:35 +0800

    Initial Commit

---
+++ NotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotificationDemo
+//
+//  Created by jjyy on 16/8/26.
+//  Copyright © 2016年 Arthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
